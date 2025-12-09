3e3583bc93f0246939c49a0aab8a83efe8744e7d
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2025-12-09 16:10:04 +0200

    hw-ts

---
+++ src/task-7.ts
@@ -0,0 +1,9 @@
+async function getMessage(): Promise<string> {
+  return await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from TS");
+    }, 1000);
+  });
+}
+
+getMessage().then((result) => console.log(result));
